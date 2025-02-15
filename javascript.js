dcf60d0b471003493585eb143022396c345c7f62
nerddaprogramacao <andradejediel70@gmail.com>
2025-02-15 12:13:18 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let imagem = document.querySelector(".troca-imagem")
+
+function trocaimagem(){
+if (imagem.style.opacity ==0){
+    imagem.style.opacity = 1
+}
+else {
+    imagem.style.opacity = 0
+}
+
+
+}
+
+setInterval (trocaimagem, 3000)
+
